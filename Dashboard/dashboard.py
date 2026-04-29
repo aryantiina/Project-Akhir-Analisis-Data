1c3a895ef59540b0e7916d0ee1049a56830e242c
aryantiina <145460588+aryantiina@users.noreply.github.com>
2026-04-29 15:08:04 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -3,11 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
+import os
 
 st.set_page_config(layout="wide", page_title="E-Commerce Data Analysis Dashboard")
 
 st.title("E-Commerce Public Dataset Analysis")
-st.write("Dashboard ini menyajikan analisis performa pengiriman dan rating pelanggan.")
+st.write("Dashboard ini menyajikan analisis performa pengiriman dan rating pelanggan.",  os.listdir())
 
 @st.cache_data
 def load_data():
